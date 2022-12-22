fd69393d86ab34b25869b103f9827eaae77f45c2
RafaelBernardinelli <rafaelbernardinelli16@gmail.com>
2022-12-21 21:49:12 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css';
+
+import React from "react";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="Banner" />
+    </header>
+  );
+}
+
+export default Banner;
